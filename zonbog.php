b8b1f8decb05eff1ec7967e5f24a7f6916122307
XxReivaaaxX <deryan765@gmail.com>
2024-11-26 14:54:24 -0500

    CC

--- zonbog.php
+++ zonbog.php
@@ -20,3 +20,14 @@ foreach ($data as $dat) {
 }  
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>HOLA</p>
+</body>
+</html>
