c7c7487c562bffcdbc2f560cc06edf92ac36fd5e
XxReivaaaxX <deryan765@gmail.com>
2024-11-26 14:47:45 -0500

    CC

---
+++ zonbog.php
@@ -0,0 +1,21 @@
+<?php
+include_once 'conexion.php';
+$objeto = new Conexion();
+$conexion = $objeto->Conectar();
+
+
+$consulta = "SELECT * FROM CART_ID"
+$resultado->execute();
+$data=$resultado->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+<?php
+foreach ($data as $dat) {
+    ?>
+
+    <tr>
+        <td><?php echo $dat['ID_CLIENTE']?></td>
+    </tr>
+    <?php
+}  
+?>
