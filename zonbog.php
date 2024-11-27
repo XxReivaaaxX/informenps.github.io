5e849ff4f80231b04c000a96a80ce99ec453f3d9
XxReivaaaxX <deryan765@gmail.com>
2024-11-27 14:59:19 -0500

    cc

--- zonbog.php
+++ zonbog.php
@@ -1,4 +1,40 @@
 <?php
+include_once 'conexion.php';
+$objeto = new Conexion();
+$conexion = $objeto->Conectar();
+
+
+$consulta = "SELECT * FROM CART_ID"
+$resultado->execute();
+$data=$resultado->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+<?php
+foreach ($data as $dat) {
+    ?>
+
+    <tr>
+        <td><?php echo $dat['ID_CLIENTE']?></td>
+    </tr>
+    <?php
+}  
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>HOLA</p>
+</body>
+</html>
+
+
+
+<?php
     $conexion=mysql_connect('CO1P84S\REP04','DWHUSER','dwhuser','Canales');
 ?>
 
