44c959063f359bf6ca5f6597fd1755280b8b1b1d
XxReivaaaxX <deryan765@gmail.com>
2025-02-04 14:08:53 -0500

    cc

--- pag_fd/inicio.php
+++ pag_fd/inicio.php
@@ -395,7 +395,7 @@
 				Informe de NPS actualizado al día 04 de febrero del 2025
 				con atenciones actualizado al día 31 de enero del 2025.
 				<br><br>
-				Enlace Excel <acc
+				Enlace Excel <a
 					href="https://falabella.sharepoint.com/sites/InformeNPS/Documentos%20compartidos/Forms/AllItems.aspx?id=%2Fsites%2FInformeNPS%2FDocumentos%20compartidos%2FInforme%20NPS&p=true&ga=1">
 					Informe NPS</a> la contraseña del archivo es 1.
 				<br><br>
