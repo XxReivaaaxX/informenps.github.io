22eb8d8d7925a2ea5f972399fb052c9d9e78ad73
Deryan Felipe Varela Lopez <deryan765@gmail.com>
2025-09-26 20:09:54 -0500

    cc

--- consultphp/conexion_bd.php
+++ consultphp/conexion_bd.php
@@ -1,17 +1,16 @@
+
 <?php
-$dsn = "SQL_PHP"; // DSN
-$conn = odbc_connect($dsn, "", ""); // No usuario ni contraseña
+$serverName = "CO1P84S\REP04"; // o el nombre de tu servidor SQL
+$connectionOptions = [
+    "Database" => "Canales",
+    "Uid" => "dwhuser", // tu usuario de SQL Server
+    "PWD" => "dwhuser" // tu contraseña
+];
 
-
-$debug = false;
+$conn = sqlsrv_connect($serverName, $connectionOptions);
 
 if (!$conn) {
-    echo "❌ Error de conexión.<br>";
-    echo odbc_errormsg();
-} else {
-    if ($debug) {
-        echo "✅ Conexión exitosa usando DSN.";
-    }
+    die("❌ Error de conexión: " . print_r(sqlsrv_errors(), true));
 }
-
 ?>
+
